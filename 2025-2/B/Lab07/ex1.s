f4bdeee73c4068e955588dfffc4d1def35f62ae1
AlexandreMeslin <meslin@puc-rio.br>
2025-09-19 01:40:15 +0000

    2025-09-18

--- 2025-2/B/Lab07/ex1.s
+++ 2025-2/B/Lab07/ex1.s
@@ -11,21 +11,82 @@ int main (void) {
   return 0;
 }
 */
+/*
+Dicionário
+Reg   Var
+rbx   pc
+eax   temp
+*/
+
 #include <stdio.h>
 
-char S2[] = {65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0};
+.data   # variáveis globais e strings constante
+#char S2[] = {65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0};
+S2: .byte 65, 108, 111, 32, 123, 103, 97, 108, 101, 114, 97, 125, 33, 0
 
-int main (void) {
-//  char *pc = S2;
-  char *pc;
-  pc = S2;
-//  while (*pc) {
-  while (*pc != 0) {
-//    printf ("%c", *pc++);
-    temp = *pc;
-    printf ("%c", temp);
-    pc++;
-  }
-  printf("\n");
-  return 0;
-}
+Sf:         .string "%c"
+MUDA_LINHA: .string "\n"
+
+
+.text   # segmento de código (texto para a CPU)
+#int main (void) {
+.globl main
+main:
+/********************************************************/
+/* mantenha este trecho aqui e nao mexa - prologo !!!   */
+  pushq   %rbp
+  movq    %rsp, %rbp
+  subq    $16, %rsp
+  movq    %rbx, -8(%rbp)  /* guarda rbx */
+  movq    %r12, -16(%rbp)  /* guarda r12 */
+/********************************************************/
+
+#//  char *pc = S2;
+#  char *pc; <== não existem variáveis em linguagem de máquina
+#  pc = S2;
+  movq    $S2, %rbx
+
+WHILE:
+#  while (*pc != 0){
+  cmpb  $0, (%rbx)
+  je    FORA_WHILE
+
+#//    printf ("%c", *pc++);
+#    temp = *pc
+  movsbl  (%rbx), %eax
+
+#    printf ("%c", temp);
+/*************************************************************/
+/* este trecho imprime o valor de %eax (estraga %eax)  */
+  movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
+  movl    %eax, %esi   /* segundo parametro  (inteiro) */
+  movl  $0, %eax
+  call  printf       /* chama a funcao da biblioteca */
+/*************************************************************/
+
+#    pc++
+  incq    %rbx
+
+#  }
+  jmp WHILE
+
+FORA_WHILE:  
+#  printf("\n");
+/*************************************************************/
+/* este trecho imprime o valor de %eax (estraga %eax)  */
+  movq    $MUDA_LINHA, %rdi    /* primeiro parametro (ponteiro)*/
+  movl    %eax, %esi   /* segundo parametro  (inteiro) */
+  movl  $0, %eax
+  call  printf       /* chama a funcao da biblioteca */
+/*************************************************************/
+
+#  return 0;
+/***************************************************************/
+/* mantenha este trecho aqui e nao mexa - finalizacao!!!!      */
+  movq  $0, %rax  /* rax = 0  (valor de retorno) */
+  movq    -16(%rbp), %r12 /* recupera r12 */
+  movq    -8(%rbp), %rbx  /* recupera rbx */
+  leave
+  ret      
+/***************************************************************/
+#}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # INF1018
-Curso de Software Básico
+## Curso de Software Básico
+
+[![License: MIT](https://img.shields.io/badge/license-MIT-green.svg)](https://opensource.org/licenses/MIT)
+![Contributors](https://img.shields.io/github/contributors/AlexandreMeslin/INF1304)
+![Open Issues](https://img.shields.io/github/issues/AlexandreMeslin/INF1304)
+![Open PRs](https://img.shields.io/github/issues-pr/AlexandreMeslin/INF1304)
+![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)
